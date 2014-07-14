17ba39131cb709ec450f611059be97184ce2ab0d
Yu Sugawara <yusuga@me.com>
2014-07-15 00:21:38 +0900

    add custumAttributes to AHKActionSheetItem

--- Classes/AHKActionSheet.h
+++ Classes/AHKActionSheet.h
@@ -93,6 +93,7 @@ typedef void(^AHKActionSheetHandler)(AHKActionSheet *actionSheet);
  */
 - (void)addButtonWithTitle:(NSString *)title image:(UIImage *)image type:(AHKActionSheetButtonType)type handler:(AHKActionSheetHandler)handler;
 
+- (void)addButtonWithTitle:(NSString *)title image:(UIImage *)image attributes:(NSDictionary*)attributes handler:(AHKActionSheetHandler)handler;
 - (void)updateItemTitle:(NSString*)title image:(UIImage*)image atIndex:(NSUInteger)index;
 
 /// Displays the action sheet.
--- Classes/AHKActionSheet.m
+++ Classes/AHKActionSheet.m
@@ -31,6 +31,7 @@ static const CGFloat kCancelButtonShadowHeightRatio = 0.333f;
 /// Used for storing button configuration.
 @interface AHKActionSheetItem : NSObject
 @property (copy, nonatomic) NSString *title;
+@property (nonatomic) NSDictionary *custumAttributes;
 @property (strong, nonatomic) UIImage *image;
 @property (nonatomic) AHKActionSheetButtonType type;
 @property (strong, nonatomic) AHKActionSheetHandler handler;
@@ -120,6 +121,9 @@ static const CGFloat kCancelButtonShadowHeightRatio = 0.333f;
     AHKActionSheetItem *item = self.items[(NSUInteger)indexPath.row];
 
     NSDictionary *attributes = item.type == AHKActionSheetButtonTypeDefault ? self.buttonTextAttributes : self.destructiveButtonTextAttributes;
+    if (item.custumAttributes) {
+        attributes = item.custumAttributes;
+    }
     NSAttributedString *attrTitle = [[NSAttributedString alloc] initWithString:item.title attributes:attributes];
     cell.textLabel.attributedText = attrTitle;
     cell.textLabel.textAlignment = [self.buttonTextCenteringEnabled boolValue] ? NSTextAlignmentCenter : NSTextAlignmentLeft;
@@ -204,8 +208,19 @@ static const CGFloat kCancelButtonShadowHeightRatio = 0.333f;
 
 - (void)addButtonWithTitle:(NSString *)title image:(UIImage *)image type:(AHKActionSheetButtonType)type handler:(AHKActionSheetHandler)handler
 {
+    [self addButtonWithTitle:title image:image attributes:nil type:type handler:handler];
+}
+
+- (void)addButtonWithTitle:(NSString *)title image:(UIImage *)image attributes:(NSDictionary *)attributes handler:(AHKActionSheetHandler)handler
+{
+    [self addButtonWithTitle:title image:image attributes:attributes type:AHKActionSheetButtonTypeDefault handler:handler];
+}
+
+- (void)addButtonWithTitle:(NSString *)title image:(UIImage *)image attributes:(NSDictionary *)attributes type:(AHKActionSheetButtonType)type handler:(AHKActionSheetHandler)handler
+{
     AHKActionSheetItem *item = [[AHKActionSheetItem alloc] init];
     item.title = title;
+    item.custumAttributes = attributes;
     item.image = image;
     item.type = type;
     item.handler = handler;
